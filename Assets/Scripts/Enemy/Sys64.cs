4e009354fd69a4adb9c970211dad49e694eafc45
VantasTheViking <72925846+VantasTheViking@users.noreply.github.com>
2022-04-05 17:49:19 -0400

    FinalBoss Added

---
+++ Assets/Scripts/Enemy/Sys64.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sys64 : EnemyBase
+{
+    [SerializeField] float dashStrength;
+    
+
+    float dashDelay;
+    [SerializeField] float dashRate;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().drag = 5;
+    }
+    private void Update()
+    {
+        EnemyUpdate();
+        if (GetAlertStatus())
+        {
+            rotateToDirection(GetPlayer().transform.position);
+            if (IsAtRange())
+            {
+                
+                if (CanShoot() && player.GetComponent<GreyBoxShooting>().enabled)
+                {
+                    ShootGrenade();
+                }
+            }
+            else
+            {
+                Dash();
+            }
+        }
+    }
+    
+    bool CanDash()
+    {
+        if(dashDelay <= Time.realtimeSinceStartup)
+        {
+            dashDelay = Time.realtimeSinceStartup + dashRate;
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+    public void Dash()
+    {
+        if (CanDash())
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.Normalize(player.transform.position - transform.position) * dashStrength, ForceMode2D.Impulse);
+        }
+    }
+    
+}
