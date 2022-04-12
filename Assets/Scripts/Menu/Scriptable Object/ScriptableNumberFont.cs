d8329fb3b177ad52187891812a00216f0125b5a8
AlexPhill <alexander.phillips@ontariotechu.net>
2022-04-11 22:00:26 -0400

    deleted some scripts that came with the main menu
    deleted the scripts that were from the tutorials that were pushed with the scripts
    hopefully it didn't mess anything up.

--- Assets/Scripts/Menu/Scriptable Object/ScriptableNumberFont.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ScriptableNumberFont : ScriptableObject
-{
-    public Sprite[] _numberSprites = new Sprite[10];
-}
